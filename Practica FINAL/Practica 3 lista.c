de5a7943b0b6b11934b35e5e2319acdaa2670536
rgarzon846 <rgarzon846@alumnos.iua.edu.ar>
2025-11-26 19:23:10 -0300

    Practica 3 lista terminado

---
+++ Practica FINAL/Practica 3 lista.c
@@ -0,0 +1,229 @@
+/*Crea un programa para gestionar un inventario de automóviles utilizando un TDA. 
+El TDA debe contener información sobre cada automóvil, incluyendo modelo, stock (entero), 
+y precio (decimal). Desarrollar las siguientes funciones para:
+
+Agregar un nuevo modelo de automóvil al inventario.
+Mostrar el inventario por consola y generar un archivo txt llamado "inventario.txt".
+Eliminar un modelo de automóvil del inventario.
+Calcular el valor total del inventario.
+Ordenar de manera por stock de menor a mayor los modelos disponibles en el inventario usando quicksort
+Diseña un menú de opciones para que el usuario pueda interactuar con el programa.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+typedef struct{
+    char marca[25];
+    char modelo[25];
+    int stock;
+    double precio;
+}Automovil;
+
+typedef struct Lista{
+    Automovil automovil;
+    struct Lista *sig;
+}Lista;
+
+void Menu();
+void AgregarAutomovil(Lista **);
+void ImprimirInventario(Lista *);
+void EliminarAutomovil(Lista **);
+double CalcularValorTotal(Lista *);
+void Mayuscula(char [25]);
+void LiberarLista(Lista **);
+int ListaVacia(Lista *);
+
+int main(){
+    Menu();
+    return 0;
+}
+
+void Menu(){
+    Lista *lista = NULL;
+    int opcion = 0;
+
+    do{
+        printf("\n*********MENU*********\n");
+        printf("\nIngrese la opcion que desee ejecutar\n");
+        printf("1) Agregar un automovil al inventario\n");
+        printf("2) Imprimir el inventario registrado\n");
+        printf("3) Eliminar un automovil del inventario\n");
+        printf("4) Calcular el total de dinero registrado\n");
+        printf("5) Salir\n");
+        scanf(" %d", &opcion);
+
+        switch(opcion){
+            case 1: char otra = 's';
+            while(otra == 's' || otra == 'S'){
+                AgregarAutomovil(&lista);
+                printf("\nDesea agregar otro automovil al inventario\?: Si(S), No(N)\n");
+                scanf(" %c", &otra);
+            }
+            break;
+            case 2: if(ListaVacia(lista)){
+                ImprimirInventario(lista);
+            }else{
+                printf("\nAun no hay automoviles registrados\n");
+            }
+            break;
+            case 3: if(ListaVacia(lista)){
+                EliminarAutomovil(&lista);
+            }else{
+                printf("\nAun no hay automoviles registrados\n");
+            }
+            break;
+            case 4: if(ListaVacia(lista)){
+                printf("\nEL dinero total registrado en el inventario es de: %.2f\n", CalcularValorTotal(lista));
+            }else{
+                printf("\nAun no hay automoviles registrados\n");
+            }
+            break;
+            case 5: if(ListaVacia(lista)){
+                LiberarLista(&lista);
+            }else{
+                printf("\nSaliendo... No hay automoviles registrados\n");
+            }
+            break;
+            default: printf("\nNo se ha introducido una opcion valida\n");
+            break;
+        }
+    }while(opcion != 5);
+}
+
+void AgregarAutomovil(Lista **lista){
+    Lista *nuevo = (void *) malloc(sizeof(Lista));
+    if(nuevo != NULL){
+        printf("\nIngrese la informacion solicitada sobre el automovil\n");
+        printf("Marca: ");
+        getchar();
+        fgets(nuevo->automovil.marca, 25, stdin);
+        nuevo->automovil.marca[strcspn(nuevo->automovil.marca, "\n")] = '\0';
+        Mayuscula(nuevo->automovil.marca);
+        printf("Modelo: ");
+        fgets(nuevo->automovil.modelo, 25, stdin);
+        nuevo->automovil.modelo[strcspn(nuevo->automovil.modelo, "\n")] = '\0';
+        Mayuscula(nuevo->automovil.modelo);
+        printf("Stock: ");
+        scanf(" %d", &nuevo->automovil.stock);
+        printf("Precio: ");
+        scanf(" %lf", &nuevo->automovil.precio);
+        nuevo->sig = NULL;
+
+        if(*lista == NULL){
+            *lista = nuevo;
+        }else{
+            Lista *aux = *lista;
+            while(aux->sig != NULL){
+                aux = aux->sig;
+            }
+            aux->sig = nuevo;
+        }
+        printf("\nEl automovil se ha registrado con exito\n");
+    }else{
+        printf("\nError al registrar nuevo automovil\n");
+    }
+}
+
+void ImprimirInventario(Lista *lista){
+    Lista *aux = lista;
+
+    printf("\n********INVENTARIO********\n");
+    printf("\n%-25s | %-25s | %-10s | %s\n", "Marca", "Modelo", "Stock", "Precio");
+    while(aux != NULL){
+        printf("%-25s | %-25s | %-10d | %.2f\n", aux->automovil.marca, aux->automovil.modelo, aux->automovil.stock, aux->automovil.precio);
+        aux = aux->sig;
+    }
+
+    aux = lista;
+
+    FILE *archivo = fopen("inventario.txt", "w");
+    if(archivo != NULL){
+        fprintf(archivo, "\n%-25s | %-25s | %-10s | %s\n", "Marca", "Modelo", "Stock", "Precio");
+        
+        while(aux != NULL){
+            fprintf(archivo, "%-25s | %-25s | %-10d | %.2f\n", aux->automovil.marca, aux->automovil.modelo, aux->automovil.stock, aux->automovil.precio);
+            aux = aux->sig;
+        }
+        fclose(archivo);
+        printf("\nEl archivo con el inventario ha sido actualizado con exito\n");
+    }else{
+        printf("\nNo se ha podido actualizar el archivo con el inventario\n");
+    }
+}
+
+void EliminarAutomovil(Lista **lista){
+    Lista *aux = *lista;
+    Lista *ant = NULL;
+    Automovil automovil;
+    char marca[25];
+    char modelo[25];
+
+    printf("\nIngrese la marca del automovil que desea eliminar: ");
+    getchar();
+    fgets(marca, 25, stdin);
+    marca[strcspn(marca, "\n")] = '\0';
+    Mayuscula(marca);
+    printf("Ingrese el modelo del automovil que desea eliminar: ");
+    fgets(modelo, 25, stdin);
+    modelo[strcspn(modelo, "\n")] = '\0';
+    Mayuscula(modelo);
+
+    while(aux != NULL && (strcmp(modelo, aux->automovil.modelo) != 0) && (strcmp(marca, aux->automovil.marca) != 0)){
+        ant = aux;
+        aux = aux->sig;
+    }
+
+    if(aux != NULL){
+        if(ant == NULL){
+            *lista = aux->sig;
+        }else{
+            ant->sig = aux->sig;
+            automovil = aux->automovil;
+            free(aux);
+            printf("\nEl automovil:\n");
+            printf("\n%-25s | %-25s | %-10s | %s\n", "Marca", "Modelo", "Stock", "Precio");
+            printf("%-25s | %-25s | %-10d | %.2f\n", automovil.marca, automovil.modelo, automovil.stock, automovil.precio);
+            printf("\nse ha eliminado\n");
+        }
+    }else{
+        printf("\nNo se ha encontrado el automovil que desea eliminar\n");
+    }
+}
+
+double CalcularValorTotal(Lista *lista){
+    Lista *aux = lista;
+    double suma = 0;
+    double porauto = 0;
+
+    while(aux != NULL){
+        porauto = aux->automovil.precio * aux->automovil.stock;
+        suma += porauto;
+        aux = aux->sig;
+    }
+
+    return suma;
+}
+
+void Mayuscula(char cadena[25]){
+    for(int i = 0; cadena[i] != '\0'; i++){
+        cadena[i] = toupper(cadena[i]);
+    }
+}
+
+void LiberarLista(Lista **lista){
+    while(*lista != NULL){
+        Lista *prox = (*lista)->sig;
+        free(*lista);
+        *lista = prox;
+    }
+}
+
+int ListaVacia(Lista *lista){
+    if(lista == NULL){
+        return 0;
+    }else{
+        return 1;
+    }
+}
